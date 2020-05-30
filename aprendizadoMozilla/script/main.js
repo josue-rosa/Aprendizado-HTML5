3f672c21498e2c6dfa2f837cf4c6c6e53caa343a
josue-rosa <josuecamponogara@hotmail.com>
2020-05-29 21:22:49 -0300

    update

---
+++ aprendizadoMozilla/script/main.js
@@ -0,0 +1,6 @@
+const meuCabecalho = document.querySelector('h1')
+meuCabecalho.textContent = 'Olá, Mundo!'
+
+document.querySelector('html').onclick = function(){
+    alert('pare de me cutucar!')
+}
