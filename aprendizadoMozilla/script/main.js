5962b0391d47440ef046b84db05ec3a6b26a65f7
josue-rosa <josuecamponogara@hotmail.com>
2020-05-29 22:29:11 -0300

    update

--- aprendizadoMozilla/script/main.js
+++ aprendizadoMozilla/script/main.js
@@ -1,5 +1,5 @@
-const meuCabecalho = document.querySelector('h1')
-meuCabecalho.textContent = 'Olá, Mundo!'
+//const meuCabecalho = document.querySelector('h1')
+//meuCabecalho.textContent = 'Olá, Mundo!'
 
 /*
 document.querySelector('html').onclick = function(){
@@ -18,4 +18,24 @@ minhaImagem.onclick = function(){
 }
 
 let meuBotao = document.querySelector('button')
-let meuCabecalho = document.querySelector('h1')
+let meuCabecalho2 = document.querySelector('h1')
+
+function defineNomeUsuario(){
+    let meuNome = prompt('por favor, digite seu nome')
+        if(!meuNome || meuNome === null){
+            defineNomeUsuario()
+        }else{
+            localStorage.setItem('nome', meuNome)
+            meuCabecalho2.innerHTML = 'Mozilla é legal, ' + meuNome
+        }
+}
+
+if(!localStorage.getItem('nome')){
+    defineNomeUsuario()
+}else{
+    let nomeGuardado = localStorage.getItem('nome')
+    meuCabecalho2.textContent = 'Mozilla é legal, ' + nomeGuardado
+}
+
+meuBotao.onclick = function(){defineNomeUsuario()}
+
