0ba4b1de0835099f579d1a2a82f72caf0a90b3d3
josue-rosa <josuecamponogara@hotmail.com>
2020-05-29 21:39:33 -0300

    update

--- aprendizadoMozilla/script/main.js
+++ aprendizadoMozilla/script/main.js
@@ -1,6 +1,21 @@
 const meuCabecalho = document.querySelector('h1')
 meuCabecalho.textContent = 'Olá, Mundo!'
 
+/*
 document.querySelector('html').onclick = function(){
     alert('pare de me cutucar!')
 }
+*/
+
+let minhaImagem = document.querySelector('img')
+minhaImagem.onclick = function(){
+    let meuSrc = minhaImagem.getAttribute('src')
+        if(meuSrc === 'imagens/firefox512.png'){
+            minhaImagem.setAttribute ('src', 'imagens/hcode512.png')
+        }else{
+            minhaImagem.setAttribute ('src','imagens/firefox512.png')
+        }
+}
+
+let meuBotao = document.querySelector('button')
+let meuCabecalho = document.querySelector('h1')
