701f91e43899b19c7fbea37144bd222aa1ff10d8
josue-rosa <josuecamponogara@hotmail.com>
2020-06-12 21:12:39 -0300

    update

--- Curso HCode/Seção 5/Projeto Tabuada/assets/js/script.js
+++ Curso HCode/Seção 5/Projeto Tabuada/assets/js/script.js
@@ -1,22 +1,28 @@
-let tabuada = document.querySelector('#tabuada tbody')
+function calculaTabuada(){
 
-let valorA = 2
+    let tabuada = document.querySelector('#tabuada tbody')
+    let valorA = parseInt(document.querySelector('#valorA').value)
 
-tabuada.innerHTML = ''
+    tabuada.innerHTML = ''
 
-for(valorB = 0; valorB <= 10; valorB++){
-    
-    let resultado = valorA * valorB
-    
-    let template = `
-            <td>${valorA}</td>
-            <td>x</td>
-            <td>${valorB}</td>
-            <td>=</td>
-            <td>${resultado}</td>
-        `
-    let tr = document.createElement('tr')
-    tr.innerHTML = template
+    for(valorB = 0; valorB <= 10; valorB++){
+        
+        let resultado = valorA * valorB
+        
+        let template = `
+                <td>${valorA}</td>
+                <td>x</td>
+                <td>${valorB}</td>
+                <td>=</td>
+                <td>${resultado}</td>
+            `
+        let tr = document.createElement('tr')
+        tr.innerHTML = template
 
-    tabuada.append(tr)
+        tabuada.append(tr)
+    }
 }
+
+calculaTabuada()
+
+document.querySelector('#valorA').addEventListener('change', calculaTabuada)
